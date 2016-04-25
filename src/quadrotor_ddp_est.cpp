62314934a117c9f05e3054888900f38e64c3eea3
Taosha Fan <fantaosha@gmail.com>
2016-04-25 01:20:27 -0500

    estimation works

--- src/quadrotor_ddp_est.cpp
+++ src/quadrotor_ddp_est.cpp
@@ -98,79 +98,74 @@ int main()
 //	Mf.block(6,6,6,6)=Mf.block(0,0,6,6);
 	Mat12 M=Mf/2;
 	Mat4 R=Mat4::Identity()/50;
-	DDP<quadrotor>::Params params(M,R,Mf);
 	double dt=0.01;
 
 	// Set up initial state
 
-	int NUM=10;
+	int NUM=5;
 	int succeed=0;
-//#pragma omp parallel for reduction(+:succeed) schedule(dynamic)  
+
 	for(int n=0;n<NUM;n++)
 	{
-			std::vector<typename quadrotor::State> xrefs=xrefs0;
-			std::vector<typename quadrotor::U> us=us0;
-			quadrotor::State x0=xrefs[0];
+		DDP<quadrotor>::Params params(M,R,Mf);
 
-			x0.g.block(0,3,3,1)-=(Vec3::Random()).normalized()*30;
-			x0.g.block(0,0,3,3)*=SO3::exp(Vec3::Random().normalized()*3);
-		//	x0.g.block(0,0,3,3)*=SO3::exp((Vec3()<<3.14,0,0).finished());
-			x0.v.head(3)-=Vec3::Random().normalized()*0;
-			x0.v.tail(3)-=Vec3::Random().normalized()*0;
-			// Set up simulator
-			
-			Sim<quadrotor> sim(sys,dt);
-			sim.init(x0,4000);
+		std::vector<typename quadrotor::State> xrefs=xrefs0;
+		std::vector<typename quadrotor::U> us=us0;
+		quadrotor::State x0=xrefs[0];
 
-			Vec4 umin=-Vec4::Ones()*0;
-			Vec4 umax=Vec4::Ones()*6;
-			
-			DDP<quadrotor> ddp(sys,dt);
+		x0.g.block(0,3,3,1)-=(Vec3::Random()).normalized()*30;
+		x0.g.block(0,0,3,3)*=SO3::exp(Vec3::Random().normalized()*3);
+	//	x0.g.block(0,0,3,3)*=SO3::exp((Vec3()<<3.14,0,0).finished());
+		x0.v.head(3)-=Vec3::Random().normalized()*0;
+		x0.v.tail(3)-=Vec3::Random().normalized()*0;
+		// Set up simulator
+		
+		Sim<quadrotor> sim(sys,dt);
+		sim.init(x0,4000);
 
-			double ts=0.02;
-			double T=36;
-			double Tp=1.5;
-			size_t SN=size_t(ts/dt+0.5);
-			size_t ND=size_t(Tp/dt+0.5)+1;
+		Vec4 umin=-Vec4::Ones()*0;
+		Vec4 umax=Vec4::Ones()*6;
+		
+		DDP<quadrotor> ddp(sys,dt);
 
-#pragma omp critical
+		double ts=0.02;
+		double T=36;
+		double Tp=1.5;
+		size_t SN=size_t(ts/dt+0.5);
+		size_t ND=size_t(Tp/dt+0.5)+1;
+
+
+		int sn=2;
+		int itr_max=10;
+		for(int i=0;i<2000;i+=sn)
+		{
+			timespec T_start, T_end;
+			clock_gettime(CLOCK_MONOTONIC,&T_start);
+			ddp.init(sim.get_state(), us, xrefs, params, umin, umax, 150);
+			ddp.iterate(itr_max,us);
+			clock_gettime(CLOCK_MONOTONIC,&T_end);
+			std::cout<<"time consumed is "<<(T_end.tv_sec-T_start.tv_sec)+(T_end.tv_nsec-T_start.tv_nsec)/1000000000.0<<"s"<<std::endl;
+		
+			for(int j=0;j<sn;j++)
 			{
-//				std::cout<<quadrotor::State::diff(sim.get_state(),xrefs[0]).transpose()<<std::endl;
-//				std::cout<<xrefs.size()<<std::endl;
-//				std::cout<<us.size()<<std::endl;
+				sim.update(us.front());
+				xrefs.erase(xrefs.begin());
+				us.erase(us.begin());
 			}
 
-			int sn=2;
-			int itr_max=15;
-			for(int i=0;i<4000;i+=sn)
+			Vec12 error=quadrotor::State::diff(sim.get_state(),xrefs[0]);
+
+			if(error.norm()<5)
 			{
-				timespec T_start, T_end;
-				clock_gettime(CLOCK_MONOTONIC,&T_start);
-				ddp.init(sim.get_state(), us, xrefs, params, umin, umax, 150);
-				ddp.iterate(itr_max,us);
-				clock_gettime(CLOCK_MONOTONIC,&T_end);
-//				std::cout<<"time consumed is "<<(T_end.tv_sec-T_start.tv_sec)+(T_end.tv_nsec-T_start.tv_nsec)/1000000000.0<<"s"<<std::endl;
-			
-				for(int j=0;j<sn;j++)
-				{
-					sim.update(us.front());
-					xrefs.erase(xrefs.begin());
-					us.erase(us.begin());
-				}
-
-				Vec12 error=quadrotor::State::diff(sim.get_state(),xrefs[0]);
-
-				if(error.norm()<5)
-				{
-					succeed++;
+				succeed++;
+				break;
+			}
+			else
+				if(error.norm()>500)
 					break;
-				}
-			}
-#pragma omp critical
-			{
-				std::cout<<quadrotor::State::diff(sim.get_state(),xrefs[0]).transpose()<<std::endl;
-			}
+		}
+
+		std::cout<<quadrotor::State::diff(sim.get_state(),xrefs[0]).transpose()<<std::endl;
 	}
-	
 	std::cout<<succeed<<" "<<NUM<<std::endl;
 }
